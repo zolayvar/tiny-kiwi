c9555c6d3ea220056f7a5c43964f9c520ecbfa59
Ben Goldhaber <goldhaber@goldhaber-macbookpro.roam.corp.google.com>
2017-05-19 22:24:32 -0700

    match id number and send to a URL

--- content.js
+++ content.js
@@ -8,11 +8,26 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/kiwi/gi, 'your mom');
-
+            var matched = text.match(/kiwi[\d]*/gi);
+            //SEND MATCHED DIGIT TO FIREBASE
+            sendToFirebase(matched[0].match(/[\d]*/gi), function(response){
+                alert(response);
+            })
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
     }
+}
+
+function sendToFirebase(id, cb){
+    var req = new XMLHttpRequest();
+    var FIREBASE_URL = "[URL]";
+    req.open("POST", FIREBASE_URL, true);
+    req.onreadystatechange = function() { //Call a function when the state changes.
+        if(req.readyState == 4 && req.status == 200) {
+            cb(req.responseText);
+        }
+    }
+    req.send(id);
 }
