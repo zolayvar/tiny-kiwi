57d668227cba7e49394ead346ad69aa77d853bbc
Stephanie Zolayvar <catorceovejas@gmail.com>
2017-05-20 13:21:04 -0700

    draw some bars

--- content.js
+++ content.js
@@ -11,22 +11,45 @@ for (var i = 0; i < elements.length; i++) {
             if (text.includes('tiny-kiwi')) {
                 var matched = text.match(/kiwi[\d]*/gi);
 
-                //SEND MATCHED DIGIT TO FIREBASE
-                sendToFirebase(matched[0].match(/[\d]*/gi), function(response){
-                    alert(response);
-                });
-
-                // Replace this line with a blue bar
-                var votebar = document.createElement('div');
-                votebar.className += 'votebar';
-                element.parentElement.appendChild(votebar);
+                // Delete the [tiny-kiwi 234235] line
                 element.textContent = '';
+
+                //SEND MATCHED DIGIT TO FIREBASE
+                // sendToFirebase(matched[0].match(/[\d]*/gi), function(response){
+                //     alert(response);
+                // });
+
+                // Add a vote bar to the bottom of the post
+                var votebar = createVoteBarElement()
+                element.parentElement.appendChild(votebar);
+                sizeVoteBar(votebar);
             }
         }
     }
 }
 
-function sendToFirebase(id, cb){
+function createVoteBarElement() {
+    var votebar = document.createElement('div');
+    votebar.className += 'votebar';
+
+    for (var i = 0; i < 10; i++) {
+        var voteBucket = document.createElement('div');
+        voteBucket.className = 'votebucket';
+        votebar.appendChild(voteBucket);
+    }
+
+    return votebar;
+}
+
+function sizeVoteBar(votebar) {
+    for (var i = 0; i < votebar.childNodes.length; i++) {
+        var voteBucket = votebar.childNodes[i];
+        var width = voteBucket.offsetWidth - 1;
+        voteBucket.setAttribute("style","left:" + width*i + "px; height:" + Math.random()*100 + "%");
+    }
+}
+
+function sendToFirebase(id, cb) {
     var req = new XMLHttpRequest();
     var FIREBASE_URL = "[URL]";
     req.open("POST", FIREBASE_URL, true);
